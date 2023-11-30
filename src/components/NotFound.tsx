8b524a4027aa955aaae6e54c74ee220ad707d568
kimD0ngjun <chickentasty0112@gmail.com>
2023-12-01 02:47:28 +0900

    [style] Not Found 메시지 추가 - Not Found 컨텐츠 디자인 종료

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -14,10 +14,18 @@ const NotFoundIcon = styled(TbZoomQuestion)`
   height: 200rem;
 `;
 
+const NotFoundMessage = styled.p`
+  color: #06283d;
+  font-size: 18rem;
+  font-weight: 500;
+  margin-top: 12rem;
+`;
+
 const NotFound = () => {
   return (
     <NotFoundWrapper>
       <NotFoundIcon />
+      <NotFoundMessage>잘못 입력했어요! 다시 검색해봐요 :D</NotFoundMessage>
     </NotFoundWrapper>
   );
 };
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { AiOutlineSearch } from "react-icons/ai";
 import { LuMapPin } from "react-icons/lu";
 
