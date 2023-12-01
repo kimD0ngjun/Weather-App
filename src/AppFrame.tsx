d11a8740ff6892a3f2e5113e022ca7232ea687e3
kimD0ngjun <chickentasty0112@gmail.com>
2023-12-01 23:45:41 +0900

    [styl] 임시 컨텐츠 추가

--- src/AppFrame.tsx
+++ src/AppFrame.tsx
@@ -1,16 +1,19 @@
 import styled from "styled-components";
 import SearchBox from "./components/SearchBox";
+import NotFound from "./components/NotFound";
 
 const AppFrameWrapper = styled.div`
-  width: 250rem;
-  padding: 25rem;
+  width: 400rem;
+  padding: 40rem;
   background-color: #ffffff;
+  border-radius: 10rem;
 `;
 
 const AppFrame = () => {
   return (
     <AppFrameWrapper>
       <SearchBox />
+      <NotFound />
     </AppFrameWrapper>
   );
 };
