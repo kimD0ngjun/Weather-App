4b2d700d41aa5f091f8fb2ab01227f907d5e7b7e
kimD0ngjun <chickentasty0112@gmail.com>
2023-12-02 18:23:09 +0900

    [WARNING] 생명주기 호출 시점 어긋나는 문제 발생

--- src/components/WeatherBoxContent.tsx
+++ src/components/WeatherBoxContent.tsx
@@ -4,14 +4,22 @@ import { useAppSelector } from "../redux/hooks";
 import { RootState } from "../redux/store";
 
 import getWeatherData from "../api/WeatherAPI";
+import { WeatherDataForm } from "../utility/inteface";
 
 const WeatherBox = styled.div``;
 
 const WeatherImage = styled.img`
   width: 150rem;
-  margin-top: 40rem;
-  margin-bottom: 30rem;
+  margin-top: 20rem;
+  margin-bottom: 20rem;
 `;
+
+// const Location = styled.div`
+//   margin-top: 5rem;
+//   text-align: center;
+//   font-size: 25rem;
+//   font-weight: 800;
+// `;
 
 const Temperature = styled.p`
   display: flex;
@@ -45,27 +53,46 @@ const Description = styled.p`
 `;
 
 const WeatherBoxContent = () => {
-  const [weatherData, setWeatherData] = useState(Object);
+  const [weatherData, setWeatherData] = useState<WeatherDataForm>({
+    location: "",
+    weather: "",
+    temperature: 0,
+    humidity: 0,
+    windSpeed: 0,
+  });
+
+  console.log(weatherData);
+
   const location = useAppSelector((state: RootState) => state.location.value);
 
-  const fetchWeatherData = async () => {
-    try {
-      const weatherData = await getWeatherData(location);
-      setWeatherData(weatherData);
-      console.log(weatherData);
-      return weatherData;
-    } catch (error) {
-      console.error("에러:", error);
-    }
-  };
-
   useEffect(() => {
-    fetchWeatherData();
+    const fetchData = async () => {
+      try {
+        const data = await getWeatherData(location);
+        setWeatherData(data);
+        console.log(data);
+      } catch (error) {
+        console.error("에러:", error);
+      }
+    };
+
+    fetchData();
   }, [location]);
 
   return (
     <WeatherBox>
-      <WeatherImage src="/assets/sun.png" />
+      {/* <Location>{weatherData.location.toUpperCase()}</Location> */}
+      {weatherData.weather === "맑음" ? (
+        <WeatherImage src="/assets/sun.png" />
+      ) : weatherData.weather === "흐림" ? (
+        <WeatherImage src="/assets/cloud.png" />
+      ) : weatherData.weather === "비" ? (
+        <WeatherImage src="/assets/rain.png" />
+      ) : weatherData.weather === "눈" ? (
+        <WeatherImage src="/assets/snowing.png" />
+      ) : weatherData.weather === "안개" ? (
+        <WeatherImage src="/assets/mist.png" />
+      ) : null}
       <Temperature>
         <TemperatureData>{weatherData.temperature}</TemperatureData>
         <TemperatureUnit>°C</TemperatureUnit>
